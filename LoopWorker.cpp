98d0f05120829951806951ad6aa18af17a1fe826
dkgydden <dennis.gydesen@carlsberg.dk>
2016-05-31 22:20:24 +0200

    Add files via upload

---
+++ LoopWorker.cpp
@@ -0,0 +1,33 @@
+#include "LoopWorker.h"
+
+
+LoopWorker::LoopWorker(unsigned long interval){
+  _jobInterval = interval;
+  _lastRun = millis();
+}
+LoopWorker::LoopWorker(unsigned long interval, String timeScale){
+  if(timeScale == "ms"){
+    _jobInterval = interval;
+  }else if(timeScale == "s"){
+    _jobInterval = interval * 1000;
+  }else if(timeScale == "m"){
+    _jobInterval = interval * 1000 * 60;
+  }else if(timeScale == "h"){
+    _jobInterval = interval * 1000 * 60 * 60;
+  }else if(timeScale == "d"){
+    _jobInterval = interval * 1000 * 60 * 60 * 24;
+  }else _jobInterval = interval;
+  
+  _lastRun = millis();
+}
+
+//check if time of jobInterval has passed since last rdy()
+bool LoopWorker::rdy(){
+  if((millis() - _lastRun) >= _jobInterval){
+    _lastRun = millis();
+    return 1;
+  }else {
+    return 0;
+  }
+}
+
---
+++ LoopWorker.h
@@ -0,0 +1,43 @@
+//
+// Created by DillerSoft, so this probaly have many flaws and may set your greenhouse on fire.
+//
+//
+// LoopWorker class is a really easy way to set up scheduled jobs on the arduino
+//
+// 31-05-2016 LoopWorker 0.1.0 - Dennis Gydesen
+//
+//
+//
+//  USER GUIDE
+// use LoopWorker(5) for set up a job running every fifth milli second
+// use LoopWorker(5, "ms") for set up a job running every fifth milli second
+// use LoopWorker(5, "s") for setting up a job running every fifth second
+// use LoopWorker(5, "m") for setting up a job running every fifth minute
+// use LoopWorker(5, "h") for setting up a job running every fifth hour
+// use LoopWorker(5, "d") for setting up a job running every fifth day
+//
+// to see if your job is supposed to run, check for boolean true with LoopWorker.rdy()
+//
+//
+//
+// Known flaws
+// As now there is no solution for when the long int runs out of numbers and get reset. This will happend every 49.7 day
+//
+//
+
+
+#include "ARDUINO.h"
+
+#ifndef LoopWorker_H
+#define LoopWorker_H
+
+class LoopWorker{
+  public:
+    LoopWorker(unsigned long interval);
+    LoopWorker(unsigned long interval, String timeScale);
+    bool rdy();                                 //see if times has passed and it is ready to work again
+  private:
+    unsigned long _jobInterval;
+    unsigned long _lastRun;
+};
+#endif
